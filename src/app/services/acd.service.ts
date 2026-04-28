9eff12a7c58c598e54fda71ceda6a71b81e2f0a3
Matias Rivas <matias.mar1988@gmail.com>
2026-04-28 12:11:29 -0300

    fix ruta environment

--- src/app/services/acd.service.ts
+++ src/app/services/acd.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 import { Acd } from '../Model/acd';
 
 @Injectable({
--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 import { Persona } from '../Model/persona.model';
 
 @Injectable({
