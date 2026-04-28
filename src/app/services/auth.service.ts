260fcb2815ac8bcf81045fd00297a7ae0b529222
Matias Rivas <matias.mar1988@gmail.com>
2026-04-28 10:39:59 -0300

    fix nuevamente con rutas relativas

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 import { LoginUsuario } from '../Model/login-usuario';
 
 @Injectable({
